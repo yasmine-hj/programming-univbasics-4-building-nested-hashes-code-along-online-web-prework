33aa135c3b796b02ca912665bbbdd0afe31440b7
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-08-30 19:38:41 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,10 @@ def bonus
    }
   }
 
-  epic_tragedy [:Montague][:hero][:status] = "Dead",
-  epic_tragedy [:Capulet][:hero][:status] = "Dead",
+  epic_tragedy[:Montague][:hero][:status] = "Dead",
+  epic_tragedy[:Capulet][:hero][:status] = "Dead"
+  
+  puts epic_tragedy
 
   
 
