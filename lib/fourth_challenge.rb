f6c978703543be3dab57b495fbd63c89abcfc04b
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-08-30 19:32:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,7 +23,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         { :name => "Steven", :age => "30", :attitude => "confused"},
-        { :name "Nurse", :age => "44", :attitude => "worried" }
+        { :name =>"Nurse", :age => "44", :attitude => "worried" }
       ]
    }
 }
